4dcf94d7afac39454ce9b6859c3dc7eb3fdc66ca
Michal Jungiewicz <michal.m.jungiewicz@gmail.com>
2014-09-09 11:32:40 +0200

    Functions to extract signatures of different institutions added

--- src/saos_tm/extractor/judgment_links.clj
+++ src/saos_tm/extractor/judgment_links.clj
@@ -7,10 +7,40 @@
   (:import java.io.File)
   (:gen-class))
 
+(defn extract-and-trim [reg s]
+  (map #(str/trim (str/replace % "\n" " "))
+    (re-seq reg s)))
+
 (defn extract-signatures-osp [s]
-  (map #(str/trim (str %))
-    (map #(first %)
-      (re-seq #"[XVI]*\s*[0-9]*\s*[A-Za-z]+\s+[0-9]+(/[0-9]+)+" s))))
+  (extract-and-trim
+    #"[IVXLCDM]+[\s\.]+[0-9]*[\s\.]*[a-zA-Z]*[\s\.]+\d+/\d+"
+    s))
+
+(defn extract-signatures-kio-no-space [s]
+  (extract-and-trim #"KIO/UZP/\d+/\d+" s))
+
+(defn extract-signatures-kio-space [s]
+  (extract-and-trim #"KIO/UZP\s+\d+/\d+" s))
+
+(defn extract-signatures-kio-uzp [s]
+  (extract-and-trim #"UZP/ZO/\d+-\d+/\d+" s))
+
+(defn not-tk? [s]
+  (or
+    (common/substring? "nr" s)
+    (common/substring? "OSNC" s)
+    (= (last s) "/")))
+
+(defn extract-signatures-tk [s]
+  (map #(subs % 1 (count %))
+    (remove #(not-tk? %)
+      (re-seq #"\s[a-zA-Z]+\s+\d+/\d+" s))))
+
+(defn extract-signatures-sn [s]
+  (extract-and-trim #"[a-zA-Z]+\s+[IVXLCDM]+-\d+-\d+/\d+" s))
+
+(defn extract-signatures-nsa [s]
+  (extract-and-trim #"[IVXLCDM]+\s+[a-zA-Z]+/[a-zA-Z]+\s+\d+/\d+" s))
 
 (defn extract-all-signatures [s]
   (let [
--- test/saos_tm/extractor/core_test.clj
+++ test/saos_tm/extractor/core_test.clj
@@ -198,3 +198,51 @@
             " dnia 22 lutego 2007 r., Tw 26/06, OTK ZU nr 2/B/2008, poz. 42).")
     ]
   (is (= (extract-signatures-osp s) '("Tw 26/06")))))
+
+(deftest extract-signatures-osp-test
+  (is (= (extract-signatures-osp
+        "IV CKN 178/01 II CSK 418/13 I 2 C 24/97")
+      ["IV CKN 178/01" "II CSK 418/13" "I 2 C 24/97"]))
+  (is (= (extract-signatures-osp
+        (str "dnia 02.02.2006 r., sygn. akt: III Ca 727/05), to tym "
+        "w wyroku z 15.06.2004 III K 38/03 - oddanie"
+        "dnia 9 lutego 2007 r. III CZP 161/06 opubl. w OSNC 1/208 poz. 4"))
+      ["III Ca 727/05" "III K 38/03" "III CZP 161/06"]))
+  (is (= (extract-signatures-osp
+        "I.2.C.24/97 II.CSK.418/13")
+      ["I.2.C.24/97" "II.CSK.418/13"]))
+  (is (= (extract-signatures-osp
+        (str "marca 2005r. sygn. akt II K.350/04 obejmującego"
+        "2004r. sygn. akt II K.184/04 obejmującego wyrok"
+        "łącznym o sygn. akt II K 350/04 z dnia 22 marca 2005 r. "))
+      ["II K.350/04" "II K.184/04" "II K 350/04"]))
+  (is (= (extract-signatures-kio-no-space
+        "KIO/UZP/1/07 KIO/UZP/102/08")
+      ["KIO/UZP/1/07" "KIO/UZP/102/08"]))
+  (is (= (extract-signatures-kio-no-space
+        (str "  sygn. akt: KIO/UZP/42/07 oraz Wyrok KIO z dnia 22.02.2008 r.,"
+        " sygn. akt: KIO/UZP/99/08) istnienie"))
+      ["KIO/UZP/42/07" "KIO/UZP/99/08"]))
+  (is (= (extract-signatures-kio-space
+        "KIO/UZP 141/08 KIO/UZP 102/08")
+      ["KIO/UZP 141/08" "KIO/UZP 102/08"]))
+  (is (= (extract-signatures-kio-space
+        (str "31.01.2008 r., sygn. akt: KIO/UZP 119/07 – przedłożony"
+          "31.01.2008 r., sygn. akt: KIO/UZP\n119/08.\n"
+          "Sygn. akt KIO/UZP 961/08\nWYROK\nz dnia 24 września 2008 r."))
+      ["KIO/UZP 119/07" "KIO/UZP 119/08" "KIO/UZP 961/08"]))
+  (is (= (extract-signatures-kio-uzp
+        "UZP/ZO/0-1094/99, UZP/ZO/0-725/05")
+      ["UZP/ZO/0-1094/99" "UZP/ZO/0-725/05"]))
+  (is (= (extract-signatures-tk
+        (str "1998 r., sygn. K 3/98, OTK ZU nr 4/1998, poz. 52; 23 lutego 1999"
+          " r., sygn. K 25/98, OTK ZU nr 2/1999, poz. 23; 19 czerwca 2002 r."
+          ", sygn. K 11/02, OTK ZU Sygn. akt K 10/09*"
+          "CZP 161/06 opubl. w OSNC 1/208 poz. 4 "))
+      ["K 3/98" "K 25/98" "K 11/02" "K 10/09"]))
+  (is (= (extract-signatures-sn
+        "BSA I-4110-4/13, BSA I-4110-5/07")
+      ["BSA I-4110-4/13" "BSA I-4110-5/07"]))
+  (is (= (extract-signatures-nsa
+        "I SA/Bd 680/14, IV SA/Gl 543/14, I SA/Rz 794/11")
+      ["I SA/Bd 680/14" "IV SA/Gl 543/14" "I SA/Rz 794/11"])))
