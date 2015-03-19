58e070841386b33726cf93b92e91048d9b147767
Michal Jungiewicz <michal.m.jungiewicz@gmail.com>
2015-03-19 17:15:58 +0100

    rich-judgment-links: one fix and begging of algorithm added

--- src/saos_tm/extractor/rich_judgment_links.clj
+++ src/saos_tm/extractor/rich_judgment_links.clj
@@ -1,8 +1,22 @@
 (ns saos-tm.extractor.rich-judgment-links
   (:require
-   [ clojure.string :as str ])
+   [ clojure.string :as str ]
+   [ saos-tm.extractor.judgment-links :refer :all ])
   (:import java.io.File)
   (:gen-class))
 
+(defn extract-other-data [case-nmb s]
+  (let [
+        parts (str/split s (re-pattern case-nmb))
+;;         _ (prn parts)
+        ]
+    ))
+
 (defn extract-ref-judgments [s]
-  '#{{:judgmentDate nil :court nil :judgmentType nil :caseNumber nil}})
+  (let [
+        case-nmbs (extract-all-signatures s)
+        other-judgment-links-data (extract-other-data (first case-nmbs) s)
+        other-judgment-links-data (map #(extract-other-data % s) case-nmbs)
+;;         _ (prn case-nmbs)
+        ]
+  #{{:judgmentDate nil :court nil :judgmentType nil :caseNumber nil}}))
