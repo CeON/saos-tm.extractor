bc95cf83ed739b874ac71992e037f97451c4f7d5
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-06-29 07:08:35 +0200

    Updating version information in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject saos-tm.extractor "0.2.0-SNAPSHOT"
+(defproject saos-tm.extractor "1.0.0"
   :description "SAOS metadata extractor module."
   :url "https://github.com/CeON/saos-tm.extractor"
   :license {:name "Eclipse Public License"
